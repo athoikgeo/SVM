418d5577c212f30bc793cac50e89a3210d576444
athoikgeo <72350919+athoikgeo@users.noreply.github.com>
2021-09-30 11:46:33 +0300

    Update 1datasetcode.py

--- 1datasetcode.py
+++ 1datasetcode.py
@@ -16,7 +16,7 @@ X_final = np.array(X_final)
 
 X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.3, random_state=42)
 
-#Έχω βάλει το PCA σε σχόλιο για να κανω υπολογισμούς με και χωρίς pca
+
 
 # from sklearn.preprocessing import StandardScaler
 # scaler = StandardScaler()
@@ -31,7 +31,6 @@ X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.3, r
 # X_train = scaler.fit_transform(X_train)
 # X_test= scaler.fit_transform(X_test)
 
-print("Ταξινόμηση των δεδομένων και υπολογισμός της απόδοσης της ταξινόμησης")
 
 clf = svm.SVC(gamma='scale')
 clf.fit(X_train, y_train)
@@ -41,14 +40,14 @@ correct = 0
 for i in range(len(X_test)):
     if y_results[i] == y_test[i]:
         correct+=1
-print("απόδοση")
+print("accuracy")
 print(correct/len(X_test))
 
-print("παραδείγματα ορθής και εσφαλμένης κατηγοριοποίησης")
+print("right and wrong classification examples")
 print(y_results == y_test)
-print("ορθή κατηγοριοποίηση")
+print("right classification example")
 print(y_results[1] == y_test[1])
-print("εσφαλμένη κατηγοριοποίηση")
+print("wrong classification example")
 print(y_results[37] == y_test[37])
 
 # In[2]:
@@ -58,7 +57,7 @@ from time import time
 from sklearn import svm
 from sklearn import metrics
 t01 = time()
-print("Αλλαγή παραμέτρων συνάρτησης πυρήνα")
+print("change kernel parameters")
 
 parameters = {'kernel':('linear', 'rbf','poly'),'C':(1,1e3, 1e4, 1e5)}
 svc = svm.SVC(gamma="scale")
@@ -70,7 +69,7 @@ print(clf.cv_results_['mean_test_score'])
 print("best parameters")
 print(clf.best_params_)
 
-print("Υπολογισμός της απόδοσης για c = 1 , linear kernel")
+print("accuracy for c = 1 , linear kernel")
 
 t02 = time()
 clf = svm.SVC(C=1, kernel='linear')
@@ -86,7 +85,7 @@ print("Accuracy score:",metrics.accuracy_score(y_test, y_pred))
 
 
 t02 = time()
-print("Αλλαγή παραμέτρων για rbf kernel")
+print("Change parameters for rbf kernel")
 param_grid = {'C': [1,1e3, 1e4, 1e5],
               'gamma': [0.0001, 0.001, 0.01, 0.1], }
 clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'),
@@ -98,7 +97,7 @@ print("Best estimator found by grid search:")
 print(clf.best_estimator_)
 print(clf.best_score_)
 
-print("Yολογισμός της απόδοσης για  C =1000 , gamma = 0.0001 kernel = rbf")
+print("Accuracy for  C =1000 , gamma = 0.0001 kernel = rbf")
 
 clf = svm.SVC(C=1000,gamma= 0.0001, kernel='rbf')
 clf = clf.fit(X_train, y_train)
@@ -113,7 +112,7 @@ print("Accuracy score:",metrics.accuracy_score(y_test, y_pred))
 
 
 t02 = time()
-print("Αλλαγή παραμέτρων για polynomial kernel")
+print("Change parameters polynomial kernel")
 param_grid = {'C': [1,1e3, 1e4, 1e5],
               'gamma': [0.0001, 0.001, 0.01, 0.1], }
 clf = GridSearchCV(SVC(kernel='poly', degree = 2, class_weight='balanced'),
@@ -125,7 +124,7 @@ print(clf.cv_results_['mean_test_score'])
 print("Best estimator found by grid search:")
 print(clf.best_estimator_)
 
-print("Yπολογισμός της απόδοσης για gamma= 0.01, C= 1 polynomial Kernel")
+print("Accuracy for gamma= 0.01, C= 1 polynomial Kernel")
 t01 = time()
 svm = SVC(kernel='poly', random_state=None, gamma=0.01, C=1, degree=2)
 svm.fit(X_train, y_train)
@@ -142,11 +141,7 @@ print("done in %0.3fs" % (time() - t0))
 print("Accuracy score:",metrics.accuracy_score(y_test, y_pred))
 
 
-# In[3]
-# O παρακάτω κώδικας υπολογίζει ξεχωριστά για την κάθε συνάρτηση πυρήνα
-#  και την κάθε παράμετρο γ και C που χρησιμοποιήθηκαν την απόδοση του καθώς και
-# τον χρόνο εκπαίδευσης και πρόβλεψης
-print(" Υπολογισμός της απόδοσης και του χρόνου εκπαίδευσης και πρόβλεψης για κάθε παραπάνω συνδιασμό παραμετρων ")
+
 from time import time
 from sklearn.model_selection import GridSearchCV
 from sklearn import svm
@@ -175,7 +170,7 @@ for theKernel in kernelType:
 print("best parameters")
 print(clf.best_params_)
 print(" Best accuracy")
-print(max(A)) # η μέγιστη απόδοση
+print(max(A)) 
 
 cValue = {1, 1e3, 1e4,1e5}
 gammaValue = {0.0001, 0.001, 0.01, 0.1}
@@ -201,7 +196,7 @@ for gamma in gammaValue:
 print("best parameters")
 print(clf.best_params_)
 print("Best accuracy")
-print(max(B)) # η μέγιστη απόδοση
+print(max(B)) 
 
 
 cValue = {1, 1e3, 1e4,1e5}
@@ -229,14 +224,14 @@ for gamma in gammaValue:
 print("best parameters")
 print(clf.best_params_)
 print("Best accuracy")
-print(max(C)) # η μέγιστη απόδοση
+print(max(C)) 
 
 print(" the best accurracy of all")
-print(max(max(A),max(B),max(C))) # η καλύτερη από όλες τις αποδόσεις
+print(max(max(A),max(B),max(C)))
 
 
 # In[4]
-print("Ταξινόμηση βάσει πλησιέστερου γείτονα")
+print("KNN Classification")
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from time import time
@@ -250,7 +245,7 @@ print("done in %0.3fs" % (time() - t0))
 print("Accuracy score:")
 print(accuracy_score(y_test,y_pred))
 
-print("Ταξινόμηση βάσει πλησιέστερου κέντρου")
+print("Near Centroid Classification")
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 clf = NearestCentroid()
 clf.fit(X_train, y_train)
